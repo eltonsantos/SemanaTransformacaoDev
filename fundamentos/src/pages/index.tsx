cacffce55b812269672c4d1b57466bb75dced39e
Elton Santos <eltonaxl@hotmail.com>
2023-05-10 02:02:33 -0300

    Initial commit

---
+++ fundamentos/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Icon24Hours } from "@tabler/icons-react";
+
+export default function Home() {
+  return (
+    <div
+      className={`
+        flex justify-center items-center h-screen
+        bg-black text-white text-6xl
+      `}
+    >
+      <Icon24Hours size={100} strokeWidth={1} />
+      <span>Fundamentos</span>
+    </div>
+  )
+}
